48fed2dee6bd99b215f4f462b71307207b36174b
Alexandre Silva <alexandresilva933@gmail.com>
2020-11-07 00:28:20 -0300

    Mais exercícios de aritmetica simples

---
+++ Python/aritmetica_simples/ex7.py
@@ -0,0 +1,8 @@
+# Calcular a média geométrica
+import  math
+
+valor1 = float(input("Valor 1 \n"))
+valor2 = float(input("Valor 2 \n"))
+media_geometrica = math.sqrt((valor1 * valor2))
+
+print(media_geometrica)
---
+++ Python/aritmetica_simples/ex8.py
@@ -0,0 +1,8 @@
+# Converter milhas marítimas em quilômetros
+
+milha_em_m = 1852
+valor = float(input("Digite a quantidade de milhas marítimas \n"))
+milha_km = (valor * milha_em_m) / 1000
+
+print(str(milha_km) + " Km")
+
